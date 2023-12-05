72978c8c83dfeccde86b7c915d48269e59e7ae5f
Julien <julien@Ubuntu22.myguest.virtualbox.org>
2023-12-05 23:41:07 +0100

    Modified Freq_inc with a model inspired by UNet. Computing optimizations needs to be done to learn on a big corpus

--- Freq_inc.py
+++ Freq_inc.py
@@ -5,6 +5,8 @@ import sys
 import matplotlib.pyplot as plt
 import torchvision.transforms as transforms
 from slice_vid import slice_video
+import cv2
+import torch.nn.functional as F
 
 # Charger une image à l'aide de slice_vid
 frames=slice_video('video_test.mp4')
@@ -29,8 +31,10 @@ for frame in frames :
     else :
         y.append(frame)
 
-X_frames_tensor=torch.tensor(X)
-y_frames_tensor=torch.tensor(y)
+X_frames=np.array(X)
+y_frames=np.array(y)
+X_frames_tensor=torch.tensor(X_frames).float()
+y_frames_tensor=torch.tensor(y_frames).float()
 
 print(X_frames_tensor.shape,y_frames_tensor.shape)
 
@@ -40,39 +44,71 @@ Nbsortie=2
 crit = nn.MSELoss()
 
 class Mod(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels, out_channels):
         super(Mod, self).__init__()
-        self.cnn = nn.Conv3d(in_channels=1, out_channels=Nbsortie, kernel_size=(3,3,3), stride=1, padding=(1,1,1))
 
-    def forward(self,x):
-        y=self.cnn(x)
-        return y
+        # Encodeur
+        self.encoder = nn.Sequential(
+            nn.Conv3d(in_channels, 64, kernel_size=(1, 3, 3), padding=(0, 1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(64, 64, kernel_size=(1, 3, 3), padding=(0, 1, 1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        )
 
-def train(mod,data,target):
+        # Bottleneck
+        self.bottleneck = nn.Sequential(
+            nn.Conv3d(64, 128, kernel_size=(1, 3, 3), padding=(0, 1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(128, 64, kernel_size=(1, 3, 3), padding=(0, 1, 1)),
+            nn.ReLU(inplace=True),
+        )
+
+        #Décodeu
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose3d(128, 64, kernel_size=(1, 2, 2), stride=(1, 2, 2), padding=(0, 0, 0)),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(64, 64, kernel_size=(1, 3, 3), padding=(0, 1, 1)),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(64, out_channels, kernel_size=(1, 3, 3), padding=(0, 1, 1)),
+            nn.ReLU(inplace=True),
+        )
+
+    def forward(self, x):
+        x1 = self.encoder(x)
+        x2 = self.bottleneck(x1)
+        x3 = self.decoder(torch.cat([x1, x2], dim=1))
+        x4 = 0.5*(x3[:,:,0,:,:]+x3[:,:,1,:,:]) 
+        return x4
+
+def train(mod,data,target,nepochs):
     optim = torch.optim.Adam(mod.parameters(), lr=0.001)
+    inputs, goldys = data , target
     for epoch in range(nepochs):
-        inputs, goldy = data , target
         optim.zero_grad()
-        haty = mod(inputs)
-        loss = crit(haty,goldy)
+        loss=0
+        for k in range(len(data)-1):
+            input=inputs[k:k+2].unsqueeze(0)
+            haty = mod(input.permute(0, 4, 1, 2, 3))
+            goldy = goldys[k].unsqueeze(0)
+            goldy = goldy.permute(0,3,1,2)
+            loss += crit(haty,goldy)
+        loss/=len(data)
+        print("err", loss)
         loss.backward()
         optim.step()
-        print("err", loss)
-    imgs = haty.cpu().detach().numpy()  # Conversion en tableau NumPy
 
-    # Afficher chaque image des tenseurs de sortie
-    for i in range(imgs.shape[2]):  # Parcourt la dimension de profondeur (depth)
-        fig, axes = plt.subplots(1, imgs.shape[1], figsize=(12, 3))  # Création d'une figure avec des sous-graphiques
-    
-    # Parcourt les canaux de sortie pour afficher chaque image
-        for j in range(imgs.shape[1]):
-            axes[j].imshow(imgs[0, j, i, :, :], cmap='gray')  # Affichage de l'image avec cmap='gray' pour les images en niveaux de gris
-            axes[j].axis('off')  # Désactive les axes
-            axes[j].set_title(f'Channel {j+1}')  # Définit un titre pour chaque image
-    
-        plt.show() 
-
-mod=Mod()
+mod=Mod(3,3)
 #print("nparms",sum(p.numel() for p in mod.parameters() if p.requires_grad),file=sys.stderr)
-#train(mod)
-print(mod(X_frames_tensor))
+train(mod,X_frames_tensor, y_frames_tensor, 10)
+output = mod(X_frames_tensor[0:2].unsqueeze(0).permute(0, 4, 1, 2, 3))
+output=output.permute(0,2,3,1)
+output_np = output.cpu().detach().numpy()
+
+# Échelle des valeurs pour les images en uint8 (0-255)
+scaled_output = ((output_np - output_np.min()) / (output_np.max() - output_np.min()) * 255).astype(np.uint8)
+print(scaled_output)
+# Afficher chaque image des tenseurs de sortie
+plt.imshow(scaled_output[0, :, :, :], cmap='gray')
+plt.title('Image 1')
+plt.show()
